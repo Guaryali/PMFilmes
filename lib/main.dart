7b791f8e3c7501e4b89b45a5bd043f39a34fc65e
unknown <gustavo.ariela.ds@gmail.com>
2024-02-21 22:46:48 -0300

    branch com o código da aula do dia Wed, Feb 21, 2024 10:46:48 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pmfilmes/page/genero_page.dart';
+import 'package:pmfilmes/repositorio/genero_repositorio.dart';
+import 'package:pmfilmes/vm/genero_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +13,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(
+        create: (_) =>  GeneroViewModel(generos: GeneroRepositorio.list()
+      ),
+      ),
+      ],
+      child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
@@ -33,25 +44,21 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => const GeneroPage()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
       appBar: AppBar(
-   
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-   
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -68,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
--- lib/vm/genero_view_model.dart
+++ lib/vm/genero_view_model.dart
@@ -3,8 +3,19 @@ import 'package:pmfilmes/dominio/genero.dart';
 
 class GeneroViewModel extends ChangeNotifier {
   final List<Genero> generos;
+  late Genero _genero; /* _ objeto privada */
 
   GeneroViewModel({
     required this.generos,
   });
+
+  void selecionaGenero(Genero genero) {
+    /* recupera um GeneroViewModel do provider com o estado
+    compartilhado na aplicação */
+    _genero = genero;
+    notifyListeners();
+  }
+
+  //Fecha a tela atual.
+  Genero get generoSelecionado => _genero;
 }
